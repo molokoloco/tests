cc124087fc309db4f623d0d2cc837befdb3421c5
Julien G <molokoloco@gmail.com>
2014-12-17 13:37:07 +0100

    re 15

--- worker.js
+++ worker.js
@@ -1,25 +1,12 @@
 
-console.log("SW startup");
+console.log('SW startup');
 /*
-    console.log("Request", self.Request);
-    console.log("Response", self.Response);
-    console.log("fetch", self.fetch);
-    console.log("Cache", self.Cache);
-    console.log("caches", self.caches);
-    console.log("getAll", self.getAll);
-
-    navigator: WorkerNavigator
-    onactivate: null
-    onerror: null
-    onfetch: null
-    ongeofenceenter: null
-    ongeofenceleave: null
-    oninstall: null
-    onmessage: null
-    onnotificationclick: null
-    onnotificationerror: null
-    onpush: null
-    onsync: null
+    console.log('Request', self.Request);
+    console.log('Response', self.Response);
+    console.log('fetch', self.fetch);
+    console.log('Cache', self.Cache);
+    console.log('caches', self.caches);
+    console.log('getAll', self.getAll);
 */
 
 var notifyMe = function() {
@@ -28,21 +15,18 @@ var notifyMe = function() {
         return;
     }
 
-    if (Notification.permission !== "granted") Notification.requestPermission();
+    if (Notification.permission !== 'granted') Notification.requestPermission();
 
     var notification = new Notification('Notification title', {
         icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
-        body: "Hey there! You've been notified from Web Worker!",
+        body: 'Hey there! You\'ve been notified from Web Worker!',
     });
 
-    // notification.onclick = function () {
-    //     window.open("http://stackoverflow.com/a/13328397/1269037");
-    // }
+    // notification.onclick = function () {}
 };
 
 self.addEventListener('install', function(event) {
     console.log('install', event);
-    //debugger;
 });
 
 self.addEventListener('activate', function(event) {
@@ -51,7 +35,7 @@ self.addEventListener('activate', function(event) {
 
     /*event.waitUntil(
         somethingThatReturnsAPromise().then(function() {
-            console.log("Activated!");
+            console.log('Activated!');
         })
     );*/
 });
@@ -59,11 +43,11 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) { // http://www.html5rocks.com/en/tutorials/service-worker/introduction/
     console.log('fetch', event);
 
-    //event.respondWith(new Response("Hello everyone!"));
+    //event.respondWith(new Response('Hello everyone!'));
 
     /*
-    console.log("Fetching", event.request.url);
-    console.log("Headers", new Set(event.request.headers));
+    console.log('Fetching', event.request.url);
+    console.log('Headers', new Set(event.request.headers));
     event.respondWith(fetch(event.request));
     */
     /*
@@ -81,22 +65,23 @@ self.addEventListener('fetch', function(event) { // http://www.html5rocks.com/en
 
 self.addEventListener('message', function(event) {
     console.log('message 1', event);
-    console.log("Got message in SW", event.data.text);
+    console.log('Got message in SW', event.data.text);
 
-    if (event.source) event.source.postMessage("Woop!");
-    else console.log("No event.source");
-    if (event.data.port) event.data.port.postMessage("Woop!");
+    if (event.source) event.source.postMessage('Woop!');
+    else console.log('No event.source');
+    if (event.data.port) event.data.port.postMessage('Woop!');
 });
 
 self.onmessage = function(event) {
-    console.log("Got message in SW", event.data.text);
+    console.log('message 2', event);
+    console.log('Got message in SW', event.data.text);
 
-    if (event.source) event.source.postMessage("Woopy!");
-    else console.log("No event.source");
-    if (event.data.port) event.data.port.postMessage("Woopa!");
+    if (event.source) event.source.postMessage('Woopy!');
+    else console.log('No event.source');
+    if (event.data.port) event.data.port.postMessage('Woopa!');
 };
 
-setInterval(function() {
-    console.log('setInterval notif');
+setTimeout(function() {
+    console.log('setTimeout notif');
     notifyMe();
 }, 20 * 1000);
